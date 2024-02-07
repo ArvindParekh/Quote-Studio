4952f4850f89883f5caceba8a996bdf642c94a11
Arvind <aruparekh@gmail.com>
2024-02-07 20:36:35 +0530

    chore: improve ui

--- src/components/quoteCard.jsx
+++ src/components/quoteCard.jsx
@@ -31,18 +31,26 @@ const QuoteCard = () => {
   return (
     <>
       {console.log(background)}
-      <div className="w-[50%] h-full  m-4 flex flex-col gap-4 pt-20">
-        <textarea
-          type="text"
-          placeholder="Enter Quote"
-          className="border rounded-md p-3"
-          onChange={(e) => setInput(e.target.value)}
-        />
+      <div className="w-[50%] h-full m-4 flex flex-col gap-5 pt-20">
+        <div className="flex flex-col gap-2">
+          <label className="font-medium">Quote</label>
+          <input 
+            type="text"
+            // placeholder="Enter Quote"
+            className="border rounded-md p-3 focus:outline-none focus:border-2 focus:border-black"
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+          />
+        </div>
+
+        <div className="flex flex-col gap-2">
+        <label className="font-medium">Author</label>
         <input
-          placeholder="Enter author name"
-          className="border rounded-md p-3"
+          // placeholder="Enter author name"
+          className="border rounded-md p-3 focus:outline-none focus:border-2 focus:border-black"
           onChange={(e) => setAuthor(e.target.value)}
         />
+        </div>
 
         <input
           type="file"
@@ -51,32 +59,47 @@ const QuoteCard = () => {
           ref={inputRef}
           onChange={handleChange}
         />
-        <button className="border rounded-md" onClick={getFileFromDevice}>
+        <button
+          className="border-none rounded-md p-2 text-base font-medium bg-black text-white mt-5"
+          onClick={getFileFromDevice}
+        >
           Select File
         </button>
-        <button className="border rounded-md" onClick={getRandomQuote}>
+
+        <button
+          className="border-none rounded-md p-2 text-base font-medium bg-black text-white mb-5"
+          onClick={getRandomQuote}
+        >
           Get Random Quote
         </button>
-        {/* Color picker */}
-        <label>Change background color: </label>
-        <input type="color" onChange={(e) => setBackground(e.target.value)} />
 
-        <label>Change text color: </label>
-        <input type="color" onChange={(e) => setTextColor(e.target.value)} />
+        {/* Color picker */}
+        <div>
+          {/* <input
+            type="color"
+            onChange={(e) => setBackground(e.target.value)}
+            className="inline-block mr-4"
+          />
+
+          <input
+            type="color"
+            onChange={(e) => setTextColor(e.target.value)}
+            className="inline-block"
+          /> */}
+        </div>
       </div>
       <div className="w-[50%] h-full  m-4 flex items-center justify-center">
         {background && (
           <div
-            className="flex flex-col items-center justify-center m-4 w-[95%] h-[80%] text-center rounded-xl px-4"
+            className="flex flex-col items-center justify-center m-4 w-[95%] h-[80%] shadow-lg text-center rounded-xl px-4"
             style={
               background[0] === "#"
                 ? { background: background, color: textColor }
                 : { background: "url(" + background + ")", color: textColor }
             }
           >
-            <h1 className="text-6xl font-bold">
-              {input}
-            </h1>
+            <h1 className="text-6xl font-bold">{input}</h1>
+
             {author !== "" && (
               <span className="mt-5 font-semibold text-2xl">-{author}</span>
             )}
