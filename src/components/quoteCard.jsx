24eb646bcdad773e8d8a8ebf8680a947e0da8b59
Arvind <aruparekh@gmail.com>
2024-02-08 18:51:31 +0530

    style: add auto tailwind class sorting with prettier

--- src/components/quoteCard.jsx
+++ src/components/quoteCard.jsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import { useState } from "react";
-import {ColorPicker} from "primereact/colorpicker"
-import html2canvas from "html2canvas"
+import { ColorPicker } from "primereact/colorpicker";
+import html2canvas from "html2canvas";
 
 const QuoteCard = () => {
   const [input, setInput] = useState("");
@@ -30,37 +30,37 @@ const QuoteCard = () => {
     setBackground(objecturl);
   }
 
-  function handleDownload(){
-    html2canvas(document.querySelector('#canvas')).then(canvas=>{
-      canvas.toBlob((blob)=>{
-         // eslint-disable-next-line no-undef 
-        const item = new ClipboardItem({"image/png": blob})
-        navigator.clipboard.write([item]).then(console.log("done"))
-      })
-    })
+  function handleDownload() {
+    html2canvas(document.querySelector("#canvas")).then((canvas) => {
+      canvas.toBlob((blob) => {
+        // eslint-disable-next-line no-undef
+        const item = new ClipboardItem({ "image/png": blob });
+        navigator.clipboard.write([item]).then(console.log("done"));
+      });
+    });
   }
 
   return (
     <>
-      <div className="w-[50%] h-full m-4 flex flex-col gap-5 pt-20">
+      <div className="m-4 flex h-full w-[50%] flex-col gap-5 pt-20">
         <div className="flex flex-col gap-2">
           <label className="font-medium">Quote</label>
-          <input 
+          <input
             type="text"
             // placeholder="Enter Quote"
-            className="border rounded-md p-3 focus:outline-none focus:border-2 focus:border-black"
+            className="rounded-md border p-3 focus:border-2 focus:border-black focus:outline-none"
             onChange={(e) => setInput(e.target.value)}
             value={input}
           />
         </div>
 
         <div className="flex flex-col gap-2">
-        <label className="font-medium">Author</label>
-        <input
-          // placeholder="Enter author name"
-          className="border rounded-md p-3 focus:outline-none focus:border-2 focus:border-black"
-          onChange={(e) => setAuthor(e.target.value)}
-        />
+          <label className="font-medium">Author</label>
+          <input
+            // placeholder="Enter author name"
+            className="rounded-md border p-3 focus:border-2 focus:border-black focus:outline-none"
+            onChange={(e) => setAuthor(e.target.value)}
+          />
         </div>
 
         <input
@@ -71,14 +71,14 @@ const QuoteCard = () => {
           onChange={handleChange}
         />
         <button
-          className="border-none rounded-md p-2 text-base font-medium bg-black text-white mt-5"
+          className="mt-5 rounded-md border-none bg-black p-2 text-base font-medium text-white"
           onClick={getFileFromDevice}
         >
           Select File
         </button>
 
         <button
-          className="border-none rounded-md p-2 text-base font-medium bg-black text-white mb-5"
+          className="mb-5 rounded-md border-none bg-black p-2 text-base font-medium text-white"
           onClick={getRandomQuote}
         >
           Get Random Quote
@@ -86,26 +86,48 @@ const QuoteCard = () => {
 
         {/* Color picker */}
         <div>
-          <ColorPicker value={background} inputStyle={{width: '30px', height: '30px', borderRadius: '5px', margin: 4}} height={4} onChange={(e)=> setBackground('#' + e.target.value)} />
+          <ColorPicker
+            value={background}
+            inputStyle={{
+              width: "30px",
+              height: "30px",
+              borderRadius: "5px",
+              margin: 4,
+            }}
+            height={4}
+            onChange={(e) => setBackground("#" + e.target.value)}
+          />
 
-          <ColorPicker value={textColor} inputStyle={{width: '30px', height: '30px', borderRadius: '5px', margin: 4}} onChange={(e)=> setTextColor('#'+ e.target.value)} />
+          <ColorPicker
+            value={textColor}
+            inputStyle={{
+              width: "30px",
+              height: "30px",
+              borderRadius: "5px",
+              margin: 4,
+            }}
+            onChange={(e) => setTextColor("#" + e.target.value)}
+          />
         </div>
       </div>
-      <div className="w-[50%] h-full  m-4 flex items-center justify-center">
+      <div className="m-4 flex  h-full w-[50%] items-center justify-center">
         {background && (
           <div
-            className="flex flex-col items-center justify-center m-4 w-[95%] h-[80%] shadow-lg text-center rounded-xl px-4"
+            className="m-4 flex h-[80%] w-[95%] flex-col items-center justify-center rounded-xl px-4 text-center shadow-lg"
             id="canvas"
             style={
               background[0] === "#"
                 ? { background: background, color: textColor }
-                : { background: "url(" + background + ")", color: textColor }
+                : {
+                    background: "url(" + background + ")",
+                    color: textColor,
+                  }
             }
           >
             <h1 className="text-6xl font-bold">{input}</h1>
 
             {author !== "" && (
-              <span className="mt-5 font-semibold text-2xl">-{author}</span>
+              <span className="mt-5 text-2xl font-semibold">-{author}</span>
             )}
           </div>
         )}
