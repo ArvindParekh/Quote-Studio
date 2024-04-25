cf26f96aad211b406cb984d8709f363d71bffa2f
Arvind <aruparekh@gmail.com>
2024-04-25 22:46:19 +0530

    chore: add more gradient combinations

--- src/components/quoteCard.jsx
+++ src/components/quoteCard.jsx
@@ -230,6 +230,69 @@ const QuoteCard = () => {
                   }}
                   onClick={handleGradientChange}
                 ></div>
+                <div
+                  className="h-10 w-10 cursor-pointer rounded-md"
+                  style={{
+                    backgroundImage: "linear-gradient(to right, orange, cyan)",
+                  }}
+                  onClick={handleGradientChange}
+                ></div>
+                <div
+                  className="h-10 w-10 cursor-pointer rounded-md"
+                  style={{
+                    backgroundImage: "linear-gradient(to right,#ff6b81, #c0cbe8)",
+                  }}
+                  onClick={handleGradientChange}
+                ></div>
+                <div
+                  className="h-10 w-10 cursor-pointer rounded-md"
+                  style={{
+                    backgroundImage: "linear-gradient(to right, #fff9b0, #b6d7a8)",
+                  }}
+                  onClick={handleGradientChange}
+                ></div>
+                <div
+                  className="h-10 w-10 cursor-pointer rounded-md"
+                  style={{
+                    backgroundImage: "linear-gradient(to right, #87bdd8, #d6bdd8)",
+                  }}
+                  onClick={handleGradientChange}
+                ></div>
+                <div
+                  className="h-10 w-10 cursor-pointer rounded-md"
+                  style={{
+                    backgroundImage: "linear-gradient(to right, #ff7979, #b3ffb3)",
+                  }}
+                  onClick={handleGradientChange}
+                ></div>
+                <div
+                  className="h-10 w-10 cursor-pointer rounded-md"
+                  style={{
+                    backgroundImage: "linear-gradient(to right, #61a3ff, #d0d0d0)",
+                  }}
+                  onClick={handleGradientChange}
+                ></div>
+                <div
+                  className="h-10 w-10 cursor-pointer rounded-md"
+                  style={{
+                    backgroundImage: "linear-gradient(to right, #b5ffb5, #61d1ff)",
+                  }}
+                  onClick={handleGradientChange}
+                ></div>
+                <div
+                  className="h-10 w-10 cursor-pointer rounded-md"
+                  style={{
+                    backgroundImage: "linear-gradient(to right, #c3e8ff, #ffcccc)",
+                  }}
+                  onClick={handleGradientChange}
+                ></div>
+                <div
+                  className="h-10 w-10 cursor-pointer rounded-md"
+                  style={{
+                    backgroundImage: "linear-gradient(to right, #ffb6c1, #ffee99)",
+                  }}
+                  onClick={handleGradientChange}
+                ></div>
               </div>
               <div>
                 <ColorPicker
